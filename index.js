fe47adf2a64443575c343c83ffbe94315483a2cc
AbideenAyodimeji <abideenoygbenro2018@gmail.com>
2023-08-09 18:56:53 -0800

    second commmit

---
+++ index.js
@@ -0,0 +1,12 @@
+// section-center swiper
+const swiper = new Swiper('.swiper', {
+  direction: 'vertical',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  autoplay: {
+    delay: 2000,
+  },
+})
